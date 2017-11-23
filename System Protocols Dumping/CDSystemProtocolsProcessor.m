a06a930c20d8d1a413d3b609fa557ea2508dff4a
史贵岭 <SHIGUILING063@pingan.com.cn>
2017-11-23 10:30:32 +0800

    兼容xocode9-iOS11 ，xcode9的SDK路径变更导致class-dump失效

--- System Protocols Dumping/CDSystemProtocolsProcessor.m
+++ System Protocols Dumping/CDSystemProtocolsProcessor.m
@@ -20,7 +20,17 @@
     
     NSTask *task = [[NSTask alloc] init];
     task.launchPath = @"/usr/bin/find";
-    task.currentDirectoryPath = [_sdkPath stringByAppendingString:@"/System/Library/Frameworks"];
+    //兼容xcode9，xcode9的库路径变更了： /Users/shiguiling063/Downloads/Xcode9.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks/
+    if([_sdkPath containsString:@"Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot"]){
+        NSArray * seperateArray = [_sdkPath componentsSeparatedByString:@"iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot"];
+        if(seperateArray.count == 2){
+            NSString * path = [NSString stringWithFormat:@"%@%@%@",seperateArray[0],@"iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk",@"/System/Library/Frameworks/"];
+            task.currentDirectoryPath = path;
+        }
+    }else{
+        task.currentDirectoryPath = [_sdkPath stringByAppendingString:@"/System/Library/Frameworks"];
+    }
+  
     task.arguments = @[@".", @"-name", @"*.h", @"-exec",
                        @"sed", @"-n", @"-e", @"s/.*@protocol[ \\t]*\\([a-zA-Z_][a-zA-Z0-9_]*\\).*/\\1/p", @"{}", @"+"];
     task.environment = @{@"LANG": @"C",
